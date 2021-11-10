72bc7cd76f2cd7ab5c20c5b3faa9d52ab368ab0e
jackie gurung <jamyang463gelek@gmail.com>
2021-11-10 22:04:14 +0545

    view Engine added +  partials for header and footer

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -1,39 +1,42 @@
 const express = require('express');
 const path = require('path')
 const app = express ();
+const hbs = require('hbs');
 
 
 // define path for express configuration 
 const publicPath = path.join(__dirname,'../public');
-const viewPath = path.join(__dirname,'../template');
+const viewPath = path.join(__dirname,'../template/views');
 const partialsPath = path.join(__dirname,'../template/partials');
 
 // Set up hbr engine and view locations 
 app.set('view engine','hbs');
 app.set('views' , viewPath);
+hbs.registerPartials(partialsPath);
 
 // Set up  static directory to serve 
 app.use(express.static(publicPath))
 
 
-
 app.get('',(req,res) => {
     res.render('index', {
-        title: 'Weather app',
+        title: 'Weather',
         name:'Jackie Lhowa'
     })
 })
 
 app.get('/about',(req,res) => {
     res.render('about', {
-        title: 'About Page',
+        title: 'About Us',
         name:'Jackie Lhowa'
     })
 })
 
 app.get('/help', (req,res) =>{
     res.render('help',{
-        message:'call 911 for help'
+        message:'call 911 for help',
+        title: 'Help',
+        name:'Jackie Lhowa'
     })
 })
 
