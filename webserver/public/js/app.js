23cf0a4f7838f6a88e21d9969a4b698af2390ce5
jackie gurung <jamyang463gelek@gmail.com>
2021-11-12 20:43:01 +0545

    displaying data in the frontend

--- webserver/public/js/app.js
+++ webserver/public/js/app.js
@@ -1,1 +1,28 @@
-console.log("Fontend js file loaded")
+console.log("Fontend js file loaded");
+
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#messageOne');
+const messageTwo = document.querySelector('#messageTwo');
+
+
+weatherForm.addEventListener('submit',(e) => {
+    e.preventDefault();
+    const location = search.value
+
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) =>{
+        response.json().then((data) => {
+            if(data.error){ 
+                messageOne.textContent = data.error;
+            }else{
+                messageOne.textContent = `Location : ${data.location}`;
+                messageTwo.textContent = `It is ${data.weatherDescription}.Temperature is ${data.temperature} degree celsius but feels like ${data.feelslike} degree celcius `;
+                console.log(data )
+            }
+        })
+    })
+})
--- webserver/src/forecast.js
+++ webserver/src/forecast.js
@@ -1,17 +1,14 @@
 const request = require('postman-request');
 
 const getWeather = (longitude,latitude,location,callback) => {
-    console.log(longitude,latitude);
     const url = `http://api.weatherstack.com/current?access_key=a1f0dc5671ae33859bdee1bf36f58dc5&query=${longitude},${latitude}`;
     request({url,json:true},(error,{body}) => {
         if(error){
             callback("Unable to connect to weather services",undefined);
         }else if(body.error){
             callback(body.error.info,undefined);
-            console.log("body error")
         }else{
             const currentData = body.current;
-            console.log("data ")
             callback(undefined, {temperature: currentData.temperature,feelslike : currentData.feelslike, weatherDescription: currentData.weather_descriptions[0],location:location})
         }
     })
